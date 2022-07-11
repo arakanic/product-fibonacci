0cdd4316465491e34d1e4f8298c74639f304b47d
jiara <ji.arakani@gmail.com>
2022-07-10 23:09:10 -0400

    first commit

---
+++ productfib.js
@@ -0,0 +1,15 @@
+function productFib(prod){
+    const result = [];
+    // find closest f(n), f(n + 1) whose product is closest to prod
+    // true if product = prod, false if not
+    // push to result
+    return result;
+}
+
+productFib(4895)  // [55, 89, true]
+productFib(5895)  // [89, 144, false]
+productFib(74049690)  // [6765, 10946, true]
+productFib(84049690)  // [10946, 17711, false]
+productFib(193864606)  // [10946, 17711, true]
+productFib(447577)  // [610, 987, false]
+productFib(602070)  // [610, 987, true]
